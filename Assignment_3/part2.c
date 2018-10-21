5b4a65e9897a3f39db408ae642cb42c0a7a688ee
Asim Waheed <asim.waheed29@gmail.com>
2018-10-22 00:46:18 +0500

    Part 2 without sleep

--- Assignment_3/part2.c
+++ Assignment_3/part2.c
@@ -12,7 +12,7 @@ int maxPeople;
 int maxStations;
 int* trains;
 int* getting_in;
-int* getting_out;
+int** getting_out;
 sem_t* train_cap;
 sem_t* move;
 sem_t mutexC;
@@ -31,25 +31,38 @@ void initializeP2(int numStations, int maxNumPeople) {
 	trains = (int*) malloc(NUM_TRAINS*sizeof(int*));
 	train_cap = (sem_t*) malloc(NUM_TRAINS*sizeof(sem_t));
 	getting_in = (int*) malloc(numStations*sizeof(int));
-	getting_out = (int*) malloc(numStations*sizeof(int));
+	getting_out = (int**) malloc(numStations*sizeof(int*));
 	move = (sem_t*) malloc(maxStations*sizeof(sem_t));
 
 	sem_init(&mutexC, 0, 1);
 	for(i = 0; i < NUM_TRAINS; i++){
 		trains[i] = i;
 		sem_init(&train_cap[i], 0, maxNumPeople);
+		getting_out[i] = (int*) malloc(maxStations*sizeof(int));
 	}
 
+	for(i = 0; i < NUM_TRAINS; i++){
+		int j;
+		for(j = 0; j < maxStations; j++){
+			getting_out[i][j] = 0;
+			// printf("%d, %d\n", i, j);	
+		}
+	}
 	for(i = 0; i < numStations; i++){
 		getting_in[i] = 0;
-		getting_out[i] = 0;
 		sem_init(&move[i], 0, 1);
 	}
 	// printf("Initialization complete\n");
 }
-void check_train(){
+void check_train(int stat, int train){
 	// printf("In check train\n");
-	sleep(1);
+	// sleep(1);
+	while(getting_in[stat]){
+
+	}
+	while(getting_out[train][stat]){
+
+	}
 	// printf("exiting check train\n");
 }
 void* train(void* pos_arg){
@@ -64,10 +77,20 @@ void* train(void* pos_arg){
 	fflush(stdout);
 
 	while(1){
-		check_train();
+		// printf("Train %d is at %d\n", id, trains[id]);
+		check_train(trains[id], id);
+		// printf("Telling %d to wait\n", (pos+1)%maxStations);
 		sem_wait(&move[(pos+1)%maxStations]);
-		sem_post(&move[(pos-1)%maxStations]);		
-		pos++;
+		int post_pos;
+		if(pos == 0){
+			post_pos = maxStations-1;
+		} else{
+			post_pos = pos-1;
+		}
+		// printf("Telling %d to post\n", (post_pos)%maxStations);
+		sem_post(&move[(post_pos)]);
+
+		pos = (pos+1)%maxStations;
 		trains[id] = pos;
 	}
 
@@ -84,14 +107,22 @@ void *goingFromToP2(void *argu) {
 	int the_train;
     struct argument *arg = (struct argument*)(argu);
     int i;
+    getting_in[arg->from]++;
 	for(i = 0; i < NUM_TRAINS; i++){
 		if(trains[i] == arg->from){
+			sem_wait(&train_cap[i]);
 			the_train = i;
+			getting_in[arg->from]--;
+			getting_out[the_train][arg->to]++;
+			// printf("User %d has gotten into train %d to get out at %d\n", arg->id, the_train, arg->to);
+			break;
 		}
 	}
 	while(trains[the_train] != arg->to){
-
+	
 	}
+	getting_out[the_train][arg->to]--;
+	sem_post(&train_cap[i]);
 	printf("%d %d %d\n", arg->id, arg->from, arg->to);
 
 }
